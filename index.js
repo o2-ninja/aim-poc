94a898131af98bd7063e07832e05851a12db7efd
o2 <o2346@hotmail.com>
2021-08-29 11:18:49 +0900

    success parse

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const http = require( 'http' );
+
 //const hostDefault = 'localhost:4502';
 const hostDefault = '192.168.100.3:4502';
 let host = null;
@@ -11,25 +13,33 @@ if( process.env.AIM_HOST ) {
 console.log( host );
 
 //http://$host/api/assets/we-retail/en/experiences.json
+//
 
-const http = require( 'http' );
+Promise.resolve().then( ( resolve, reject ) => {
 
-const request = http.request(
-  String().concat(
-    'http://',
-    host,
-    '/api/assets/we-retail/en/experiences.json'
-  ),
-  {
-    'auth': 'admin:admin'
-  },
-  ( response ) => {
-    //console.log('STATUS: ' + response.statusCode);
-    //console.log('HEADERS: ' + JSON.stringify(response.headers));
-    response.setEncoding( 'utf8' );
-    response.on( 'data', ( chunk ) => {
-      console.log( chunk );
-    } );
-  } );
+  const request = http.request(
+    String().concat(
+      'http://',
+      host,
+      '/api/assets/we-retail/en/experiences.json'
+    ),
+    {
+      'auth': 'admin:admin'
+    },
+    ( response ) => {
+      //console.log('STATUS: ' + response.statusCode);
+      //console.log('HEADERS: ' + JSON.stringify(response.headers));
+      const output = [];
+      response.setEncoding( 'utf8' );
+      response
+        .on( 'data', ( chunk ) => {
+          //console.log( chunk );
+          output.push( chunk );
+        } )
+        .on( 'end', () => {
+          console.log( JSON.parse( output.join( '' ) ) );
+        } );
+    } )
+    .end();
 
-request.end();
+} );
